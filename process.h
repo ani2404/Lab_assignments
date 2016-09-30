8047d51430dda7af43b4dfcfef0feec0ec96833a
ani2404 <ani2404@ufl.edu>
2016-09-30 19:42:50 -0400

    added a new state to process

--- process.h
+++ process.h
@@ -16,6 +16,7 @@
 #define	PR_SUSP		5	/* Process is suspended			*/
 #define	PR_WAIT		6	/* Process is on semaphore queue	*/
 #define	PR_RECTIM	7	/* Process is receiving with timeout	*/
+#define PR_RECQ         8       /* Process waiting for message in a queue */
 
 /* Miscellaneous process definitions */
 
