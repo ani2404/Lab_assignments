b5c6da02973f890840af80b12db7b62bd12b2fda
ani2404 <ani2404@ufl.edu>
2016-09-29 23:25:10 -0400

    Debugging

--- main.c
+++ main.c
@@ -5,19 +5,9 @@
 process	main(void)
 {
 
-	/* Run the Xinu shell */
+	kprintf("Size of process entry table is %d \n",sizeof(struct procent));
 
-	recvclr();
-	resume(create(shell, 8192, 50, "shell", 1, CONSOLE));
-
-	/* Wait for shell to exit and recreate it */
-
-	while (TRUE) {
-		receive();
-		sleepms(200);
-		kprintf("\n\nMain process recreating shell\n\n");
-		resume(create(shell, 4096, 20, "shell", 1, CONSOLE));
-	}
+	
 	return OK;
     
 }
