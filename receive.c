8b015ca7ccd7b5c9a8395bf93ff795e9f87615bf
ani2404 <ani2404@ufl.edu>
2016-10-04 17:42:21 -0400

    moved to pointers

--- receive.c
+++ receive.c
@@ -30,24 +30,24 @@ umsg32 receiveMsg(void)
 	intmask	mask;			/* Saved interrupt mask		*/
 	struct	procent *prptr;		/* Ptr to process's table entry	*/
 	umsg32	msg;			/* Message to return		*/
-	uint32 head;
+	uint32 *head = &prptr->qptr->head;
 	
 	mask = disable();
 	prptr = &proctab[currpid];
 	
-	if (prptr->qptr->head == MAX_MSGS) {
+	if (*head == MAX_MSGS) {
 		// Queue is Empty
 		prptr->prstate = PR_RECQ;
 		prptr->qptr->count = 1;
 		resched();		/* Block until message arrives	*/
 	}
 	
-	head = prptr->qptr->head;
-	msg = prptr->qptr->msgq[head];		/* Retrieve message		*/
-	prptr->qptr->head = (++head % MAX_MSGS);
-	if(prptr->qptr->head == prptr->qptr->tail){
+	
+	msg = prptr->qptr->msgq[*head++];		/* Retrieve message		*/
+	*head %= MAX_MSGS;
+	if(*head == prptr->qptr->tail){
 		// Queue is now empty
-		prptr->qptr->head = MAX_MSGS;
+		*head = MAX_MSGS;
 	}
 	restore(mask);
 	return msg;
@@ -61,12 +61,12 @@ syscall receiveMsgs(umsg32* msgs, uint32 msg_count)
 	intmask	mask;			/* Saved interrupt mask		*/
 	struct	procent *prptr;		/* Ptr to process's table entry	*/
 	uint32	loop_index = 0;
+	uint32 *head = &prptr->qptr->head;
+	uint32 *tail = &prptr->qptr->tail;
 	
 	mask = disable();
 	prptr = &proctab[currpid];
-	uint32 &head = prptr->qptr->head;
-	uint32 &tail = prptr->qptr->tail;
-	if ((head == MAX_MSGS) || ((head != tail) && (((tail - head + MAX_MSGS)%MAX_MSGS) < msg_count))) {
+	if ((*head == MAX_MSGS) || ((*head != *tail) && (((*tail - *head + MAX_MSGS)%MAX_MSGS) < msg_count))) {
 		// Queue is Empty or not sufficient to conusme [msg_count] messgs
 		prptr->prstate = PR_RECQ;
 		prptr->qptr->count = msg_count;
@@ -76,15 +76,15 @@ syscall receiveMsgs(umsg32* msgs, uint32 msg_count)
 	//head = prptr->qptr->head;	
 	for(;loop_index < msg_count; loop_index++)
 	{
-		msgs[loop_index] = prptr->qptr->msgq[head++];		/* Retrieve message		*/
-		head %= MAX_MSGS;
+		msgs[loop_index] = prptr->qptr->msgq[*head++];		/* Retrieve message		*/
+		*head %= MAX_MSGS;
 	}
 	
 	//prptr->qptr->head = head;
 	
-	if(head == prptr->qptr->tail){
+	if(*head == *tail){
 		// Queue is now empty
-		prptr->qptr->head = MAX_MSGS;
+		*head = MAX_MSGS;
 	}
 	
 	restore(mask);
