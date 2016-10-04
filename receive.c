667fa9c5b002060b0e861b0f7d0e71db93828b97
ani2404 <ani2404@ufl.edu>
2016-10-04 17:32:55 -0400

    using references

--- receive.c
+++ receive.c
@@ -60,12 +60,12 @@ syscall receiveMsgs(umsg32* msgs, uint32 msg_count)
 {
 	intmask	mask;			/* Saved interrupt mask		*/
 	struct	procent *prptr;		/* Ptr to process's table entry	*/
-	uint32	head,tail,loop_index = 0;
+	uint32	loop_index = 0;
 	
 	mask = disable();
 	prptr = &proctab[currpid];
-	head = prptr->qptr->head;
-	tail = prptr->qptr->tail;
+	uint32 &head = prptr->qptr->head;
+	uint32 &tail = prptr->qptr->tail;
 	if ((head == MAX_MSGS) || ((head != tail) && (((tail - head + MAX_MSGS)%MAX_MSGS) < msg_count))) {
 		// Queue is Empty or not sufficient to conusme [msg_count] messgs
 		prptr->prstate = PR_RECQ;
@@ -73,16 +73,16 @@ syscall receiveMsgs(umsg32* msgs, uint32 msg_count)
 		resched();		/* Block until message arrives	*/
 	}
 	
-	head = prptr->qptr->head;	
+	//head = prptr->qptr->head;	
 	for(;loop_index < msg_count; loop_index++)
 	{
 		msgs[loop_index] = prptr->qptr->msgq[head++];		/* Retrieve message		*/
 		head %= MAX_MSGS;
 	}
 	
-	prptr->qptr->head = head;
+	//prptr->qptr->head = head;
 	
-	if(head == tail){
+	if(head == prptr->qptr->tail){
 		// Queue is now empty
 		prptr->qptr->head = MAX_MSGS;
 	}
