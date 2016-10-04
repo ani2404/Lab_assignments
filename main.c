b66d15819b8a35421187361852ace43915ef1491
ani2404 <ani2404@ufl.edu>
2016-10-04 03:50:46 -0400

    removed compile/run time errors

--- main.c
+++ main.c
@@ -2,8 +2,8 @@
 
 #include <xinu.h>
 
-pid32 p1,p2,p3;
-sid32 s1,s2,s3;
+pid32 p1,p2,p3,p4;
+sid32 s1,s2;
 
 process process1(void)
 {
@@ -33,56 +33,59 @@ process process4(void)
 process process2(void)
 {
 	uint32 msg_count = 1;
+	umsg32 msg_buff[MAX_MSGS]={0};
 	while(msg_count < MAX_MSGS)
 	{
-		wait(s1);
-		umsg32 msg = (umsg32)"A";
-		kprintf("Sender[1]: Sending Message "%d" to Receiver[1] \n",msg);
-		int32 result = sendMsg(p1,msg);
+		//wait(s1);
+		umsg32 msg = 21;
+		kprintf("Sender[1]: Sending Message '%d' to Receiver[1] \n",msg);
+		uint32 result = sendMsg(p1,msg);
 		if(result)
-			kprintf("Sender[1]: Message "%d" successfully sent to Receiver[1] \n",msg);
+			kprintf("Sender[1]: Message '%d' successfully sent to Receiver[1] \n",msg);
 		else
 			kprintf("Sender[1]: Unable to send message as the Receiver[1] queue is full \n");
 	
 	
-	    	umsg32 msg_buff[msg_count] = {0};
+	    	//umsg32 msg_buff[msg_count] = {0};
 		uint32 i = 0;
 		while(i < msg_count){
 			msg_buff[i] = i;
-			kprintf("Sender[1]: Sending Message "%d" to Receiver[2] \n",msg_buff[i++]);
+			kprintf("Sender[1]: Sending Message '%d' to Receiver[1] \n",msg_buff[i++]);
 		}
 		
-		uint32 result = sendMsgs(p2,msg_buff,msg_count);
+		result = sendMsgs(p1,msg_buff,msg_count);
 		
-		kprintf("Sender[1]: Successfuly sent %u messages of %u messages to Receiver[2] \n",result,msg_count);
+		kprintf("Sender[1]: Successfuly sent %u messages of %u messages to Receiver[1] \n",result,msg_count);
 		msg_count++;	
-		signal(s2);
+		//signal(s2);
 	}
+   	return OK;
 }
 
 process process3(void)
 {
 	uint32 msg_count = 1;
+	umsg32 msg_buff[MAX_MSGS]={0};
 	while(msg_count < MAX_MSGS)
 	{
 		wait(s2);
-		umsg32 msg = (umsg32)"N";
-		kprintf("Sending Message "%d" to Process[%d] \n",msg,p2);
-		int32 result = sendMsg(p2,msg);
+		umsg32 msg = 12;
+		kprintf("Sender[2]: Sending Message '%d' to Receiver[2] \n",msg);
+		uint32 result = sendMsg(p2,msg);
 		if(result)
-			kprintf("Sender[2]: Message "%d" successfully sent to Receiver[2] \n",msg);
+			kprintf("Sender[2]: Message '%d' successfully sent to Receiver[2] \n",msg);
 		else
 			kprintf("Sender[2]: Unable to send message as the Receiver[2] queue is full \n");
 	
 	
-	    	umsg32 msg_buff[msg_count] = {0};
+	    	//umsg32 msg_buff[msg_count] = {0};
 		uint32 i = 0;
 		while(i < msg_count){
 			msg_buff[i] = i;
-			kprintf("Sender[2]: Sending Message "%d" to Receiver[1] \n",msg_buff[i++]);
+			kprintf("Sender[2]: Sending Message '%d' to Receiver[1] \n",msg_buff[i++]);
 		}
 		
-		uint32 result = sendMsgs(p1,msg_buff,msg_count);
+		result = sendMsgs(p1,msg_buff,msg_count);
 		
 		kprintf("Sender[2]: Successfuly sent %u messages of %u messages to Receiver[1] \n",result,msg_count);
 		msg_count++;	
@@ -91,6 +94,7 @@ process process3(void)
 	
 	kill(p1);
 	kill(p4);
+	return OK;
 	
 }
 
@@ -102,16 +106,16 @@ process	main(void)
 	p3 = create(process3, 8192, 50, "process3", 0);
 	p4 = create(process4, 8192, 51, "process4", 0);
 
-	if(isbadpid(p1) || isbadpid(p2) || isbadpid(p32) || isbadpid(p4))
+	if(isbadpid(p1) || isbadpid(p2) || isbadpid(p3) || isbadpid(p4))
 	{
 		kprintf("Unable to create process in main \n");
 		return SYSERR;
 	}
 	
-	s1 = sem_create(1);
-	s2 = sem_create(0);
+	s1 = semcreate(1);
+	s2 = semcreate(0);
 	
-	if((s1 == (sid32)SYSERR) || (s1 == (sid32)SYSERR))
+	if((s1 == (sid32)SYSERR) || (s2 == (sid32)SYSERR))
 	{
 		kprintf("Unable to create semaphore in main \n");
 		return SYSERR;
